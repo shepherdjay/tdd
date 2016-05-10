1c02f7aded85a010d6b19f216958a5b371d99234
Jay Shepherd <jdshep89@hotmail.com>
2016-05-09 23:17:53 -0600

    Bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def safe_close(self):
         # For django bug https://code.djangoproject.com/ticket/21227
         self.browser.refresh()
