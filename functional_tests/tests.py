4de72eeaba3136c14a6463ddf438cf3d8add5763
Jay Shepherd <jdshep89@hotmail.com>
2016-05-11 20:12:37 -0600

    reverting the super changes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,13 +16,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
                 return
-        super(StaticLiveServerTestCase, cls).setUpClass()
+        super().setUpClass()
         cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
         if cls.server_url == cls.live_server_url:
-            super(StaticLiveServerTestCase, cls).tearDownClass()
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
