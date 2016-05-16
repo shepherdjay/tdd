915937f1b5ded5f0885d4f3ac7ff43b44127cd89
Jay Shepherd <jdshep89@hotmail.com>
2016-05-16 16:30:31 -0600

    Refactor list view to handle new item POSTs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,7 +3,6 @@ from unittest import expectedFailure
 
 
 class ItemValidationTest(FunctionalTest):
-    @expectedFailure
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
