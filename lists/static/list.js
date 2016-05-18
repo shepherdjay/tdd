47a154fd676f044e20d91ebba4b56ff8e424bbd4
Jay Shepherd <jdshep89@hotmail.com>
2016-05-17 18:44:50 -0600

    test adjustment

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,3 +1,5 @@
-$('input').on('keypress', function () {
-    $('.has-error').hide();
+$(document).ready(function () {
+    $('input').on('keypress', function () {
+        $('.has-error').hide();
+    });
 });
