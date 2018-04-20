c287aeac33d361c60e50c6eae70035932c23ddad
Jay Shepherd <shepherdjay@users.noreply.github.com>
2018-04-19 22:29:40 -0600

    Convert functional tests to pytest
    Created a pytest fixture for passing in a chrome browser and passing
    through arguments to it for ease of understanding.
    Update conftest.py
    Remove pytest version pinning

--- functional_tests/test_layout_and_styling.py
+++ functional_tests/test_layout_and_styling.py
@@ -1,26 +1,17 @@
-from .base import FunctionalTest
+import pytest
 
 
-class LayoutAndStylingTest(FunctionalTest):
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
+def test_layout_and_styling(live_server, browser):
+    # Edith goes to the home page
+    browser.get(live_server.url)
+    browser.set_window_size(1024, 768)
 
-        # She notices the input box is nicely centered
-        inputbox = self.get_item_input_box()
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
+    # She notices the input box is nicely centered
+    inputbox = browser.get_item_input_box()
+    assert inputbox.location['x'] + inputbox.size['width'] / 2 == pytest.approx(512, abs=10)
 
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        inputbox.send_keys('testing\n')
-        inputbox = self.get_item_input_box()
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=10
-        )
+    # She starts a new list and sees the input is nicely
+    # centered there too
+    inputbox.send_keys('testing\n')
+    inputbox = browser.get_item_input_box()
+    assert inputbox.location['x'] + inputbox.size['width'] / 2 == pytest.approx(512, abs=10)
--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,62 +1,57 @@
-from .base import FunctionalTest
+def test_cannot_add_empty_list_items(live_server, browser):
+    # Edith goes to the home page and accidentally tries to submit
+    # an empty list item. She hits Enter on the empty input box
+    browser.get(live_server.url)
+    browser.get_item_input_box().send_keys('\n')
+
+    # The home page refreshes, and there is an error message saying
+    # that list items cannot be blank
+    error = browser.get_error_element()
+    assert "You can't have an empty list item" == error.text
+
+    # She tries again with some text for the item, which now works
+    browser.get_item_input_box().send_keys('Buy milk\n')
+    browser.check_for_row_in_list_table('1: Buy milk')
+
+    # Perversely, she now decides to submit a second blank list item
+    browser.get_item_input_box().send_keys('\n')
+
+    # She receives a similar warning on the list page
+    browser.check_for_row_in_list_table('1: Buy milk')
+    error = browser.get_error_element()
+    assert "You can't have an empty list item" == error.text
+
+    # And she can correct it by filling some text in
+    browser.get_item_input_box().send_keys('Make tea\n')
+    browser.check_for_row_in_list_table('1: Buy milk')
+    browser.check_for_row_in_list_table('2: Make tea')
 
 
-class ItemValidationTest(FunctionalTest):
-    def get_error_element(self):
-        return self.browser.find_element_by_css_selector('.has-error')
+def test_cannot_add_duplicate_items(live_server, browser):
+    # Edith goes to the home page and starts a new list
+    browser.get(live_server.url)
+    browser.get_item_input_box().send_keys('Buy wellies\n')
+    browser.check_for_row_in_list_table('1: Buy wellies')
 
-    def test_cannot_add_empty_list_items(self):
-        # Edith goes to the home page and accidentally tries to submit
-        # an empty list item. She hits Enter on the empty input box
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('\n')
+    # She accidentally tries to enter a duplicate item
+    browser.get_item_input_box().send_keys('Buy wellies\n')
 
-        # The home page refreshes, and there is an error message saying
-        # that list items cannot be blank
-        error = self.get_error_element()
-        self.assertEqual(error.text, "You can't have an empty list item")
+    # She sees a helpful error message
+    browser.check_for_row_in_list_table('1: Buy wellies')
+    error = browser.get_error_element()
+    assert "You've already got this in your list" == error.text
 
-        # She tries again with some text for the item, which now works
-        self.get_item_input_box().send_keys('Buy milk\n')
-        self.check_for_row_in_list_table('1: Buy milk')
 
-        # Perversely, she now decides to submit a second blank list item
-        self.get_item_input_box().send_keys('\n')
+def test_error_messages_are_cleared_on_input(live_server, browser):
+    # Edith starts a new list in a way that causes a validation error:
+    browser.get(live_server.url)
+    browser.get_item_input_box().send_keys('\n')
+    error = browser.get_error_element()
+    assert error.is_displayed()
 
-        # She receives a similar warning on the list page
-        self.check_for_row_in_list_table('1: Buy milk')
-        error = self.get_error_element()
-        self.assertEqual(error.text, "You can't have an empty list item")
+    # She starts typing in the input box to clear the error
+    browser.get_item_input_box().send_keys('a')
 
-        # And she can correct it by filling some text in
-        self.get_item_input_box().send_keys('Make tea\n')
-        self.check_for_row_in_list_table('1: Buy milk')
-        self.check_for_row_in_list_table('2: Make tea')
-
-    def test_cannot_add_duplicate_items(self):
-        # Edith goes to the home page and starts a new list
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('Buy wellies\n')
-        self.check_for_row_in_list_table('1: Buy wellies')
-
-        # She accidentally tries to enter a duplicate item
-        self.get_item_input_box().send_keys('Buy wellies\n')
-
-        # She sees a helpful error message
-        self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.get_error_element()
-        self.assertEqual(error.text, "You've already got this in your list")
-
-    def test_error_messages_are_cleared_on_input(self):
-        # Edith starts a new list in a way that causes a validation error:
-        self.browser.get(self.server_url)
-        self.get_item_input_box().send_keys('\n')
-        error = self.get_error_element()
-        self.assertTrue(error.is_displayed())
-
-        # She starts typing in the input box to clear the error
-        self.get_item_input_box().send_keys('a')
-
-        # She is pleased to see that the error message disappears
-        error = self.get_error_element()
-        self.assertFalse(error.is_displayed())
+    # She is pleased to see that the error message disappears
+    error = browser.get_error_element()
+    assert not error.is_displayed()
