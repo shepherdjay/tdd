6622b2a67e444e572a9c6e44c459349a98723c31
Jay Shepherd <shepherdjay@users.noreply.github.com>
2023-06-07 10:18:17 -0400

    migrations

--- lists/migrations/0004_item_list.py
+++ lists/migrations/0004_item_list.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='item',
             name='list',
-            field=models.ForeignKey(default=None, to='lists.List'),
+            field=models.ForeignKey(default=None, to='lists.List', on_delete=models.CASCADE),
         ),
     ]
--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+import collections.abc
+collections.Iterator = collections.abc.Iterator
+collections.Mapping = collections.abc.Mapping
+collections.Iterable = collections.abc.Iterable
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "superlists.settings")
 
